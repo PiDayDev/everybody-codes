9e8bb491b967b60fa5e2d5e34406ca3d56a7fff7
Damiano Salvi <salvi@intre.it>
2025-12-16 10:50:37 +0100

    ⚔️ 2024 - Quest 4

---
+++ src/main/kotlin/it/pidaydev/y24/Quest4.kt
@@ -0,0 +1,23 @@
+package it.pidaydev.y24
+
+import it.pidaydev.common.quest
+import kotlin.math.abs
+
+private val quester = YEAR quest 4 withRowParser { it.toLong() }
+
+fun main() {
+
+    fun List<Long>.sumOfDiffFromMin(): Long {
+        val min = min()
+        return sumOf { it - min }
+    }
+
+    fun part1() = quester.read(1).sumOfDiffFromMin()
+    fun part2() = quester.read(2).sumOfDiffFromMin()
+    fun part3() = quester.read(3).run {
+        val median = sorted()[size / 2]
+        sumOf { abs(it - median) }
+    }
+
+    quester.verifyAndPrint(::part1, ::part2, ::part3)
+}
