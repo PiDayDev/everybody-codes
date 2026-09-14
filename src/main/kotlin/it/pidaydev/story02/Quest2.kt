dcb5ab7d99c0d7915f7f1b3591f531236eb220fc
Damiano Salvi <salvi@intre.it>
2026-09-14 14:33:21 +0200

    ⚔️ Story 2 - Quest 2

--- src/main/kotlin/it/pidaydev/story02/Quest2.kt
+++ src/main/kotlin/it/pidaydev/story02/Quest2.kt
@@ -3,17 +3,55 @@ package it.pidaydev.story02
 import it.pidaydev.common.quest
 
 private val quester = STORY quest 2 withParser {
-    // TODO
+    it.first()
 }
+
+private class BalloonLine(val balloons: String) {
+    fun pop(): Int {
+        val bolts = bolts().iterator()
+        val remainingBalloonsSequence = generateSequence(balloons) { remaining ->
+            val bolt = bolts.next()
+            remaining.dropWhile { c -> c == bolt }.drop(1)
+        }
+            .takeWhile { it.isNotEmpty() }
+        return remainingBalloonsSequence.count()
+    }
+}
+
+private class BalloonRing(val balloons: String, val times: Int) {
+    fun pop(): Int {
+        val bolts = bolts().iterator()
+        val length = balloons.length
+        val totalLength = length * times
+        var index1 = 0
+        var index2doubled = totalLength
+        var count = 0
+        val removed = mutableSetOf<Int>()
+        while (index1 < totalLength) {
+            if (index1 * 2 !in removed) {
+                val balloon = balloons[index1 % length]
+                if (balloon == bolts.next() && index2doubled % 2 == 0) {
+                    removed += index2doubled
+                    index2doubled++
+                }
+                index2doubled++
+                count++
+            }
+            index1++
+        }
+        return count
+    }
+}
+
+private fun bolts() = generateSequence { "RGB".asSequence() }.flatten()
 
 fun main() {
 
-    fun part1() = quester.read(1)
-    fun part2() = quester.read(2)
-    fun part3() = quester.read(3)
+    fun part1() = BalloonLine(quester.read(1)).pop()
 
-    quester.printAndVerify(::part1
-//        , ::part2
-//        , ::part3
-    )
+    fun part2() = BalloonRing(quester.read(2), 100).pop()
+
+    fun part3() = BalloonRing(quester.read(3), 100000).pop()
+
+    quester.printAndVerify(::part1, ::part2, ::part3)
 }
